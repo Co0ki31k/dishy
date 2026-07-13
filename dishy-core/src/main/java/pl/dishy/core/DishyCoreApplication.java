56db025a8937b215030721b358d55ff0200d56cc
Co0ki31k <mateusz.sempkowski@gmail.com>
2026-07-13 23:00:17 +0200

    Initialize dishy-core project structure with Gradle setup and basic configuration files

---
+++ dishy-core/src/main/java/pl/dishy/core/DishyCoreApplication.java
@@ -0,0 +1,13 @@
+package pl.dishy.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishyCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DishyCoreApplication.class, args);
+    }
+
+}
---
+++ dishy-core/src/test/java/pl/dishy/core/DishyCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dishy.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishyCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
